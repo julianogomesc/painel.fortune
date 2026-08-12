dd4b55a5bfd0ef4be9cd0df8470755eec4828ab1
julianogomesc <julianogomesc1988@gmail.com>
2026-08-12 13:24:03 -0300

    feat: loading component no componente de categorias da home

--- app/components/CloudCategories.vue
+++ app/components/CloudCategories.vue
@@ -12,7 +12,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="bg-gray-200 flex flex-col justify-between h-full rounded-2xl p-8 text-black bg-[url('./images/maspireside.webp')] bg-no-repeat bg-right bg-size-[55%] lg:bg-size-[45%] 2xl:pr-48">
+  <div class="bg-gray-200 flex flex-col justify-between h-full rounded-2xl p-8 text-black bg-[url('./images/maspireside.webp')] bg-no-repeat bg-right bg-size-[55%] lg:bg-size-[45%] 2xl:pr-48 relative">
+    <Loading v-if="pending" />
     <div>
       <span class="block text-gray-600 px-2 text-sm mb-3">Categorias Registradas:</span>
       <UButton v-for="cat in result" :to="`/produtos/categorias/editar/${cat.id}`" class="cursor-pointer mr-3 mb-2" :label="cat.nome" variant="soft" color="neutral" />      

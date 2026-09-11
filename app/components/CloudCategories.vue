823e081032a833a85681625f1e8fadeeeb57dd86
Juliano Gomes da Costa <julianogomesdacosta@MacBook-Air-de-Juliano.local>
2026-09-11 11:44:26 -0300

    fix: ajuste de imagem de fundo nos componentes

--- app/components/CloudCategories.vue
+++ app/components/CloudCategories.vue
@@ -12,7 +12,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="bg-gray-200 flex flex-col justify-between h-full rounded-2xl p-8 text-black bg-[url('./images/maspireside.webp')] bg-no-repeat bg-right bg-size-[55%] lg:bg-size-[45%] 2xl:pr-48 relative">
+  <div class="bg-gray-200 flex flex-col justify-between h-full rounded-2xl p-8 text-black bg-[url('/images/maspireside.webp')] bg-no-repeat bg-right bg-size-[55%] lg:bg-size-[45%] 2xl:pr-48 relative">
     <Loading v-if="pending" />
     <div>
       <span class="block text-gray-600 px-2 text-sm mb-3">Categorias Registradas:</span>
--- app/components/WelcomeUser.vue
+++ app/components/WelcomeUser.vue
@@ -3,7 +3,7 @@ const store = useLoginStore()
 </script>
 
 <template>
-  <div class="bg-blueFortune/80 flex flex-col justify-between min-h-62 h-full rounded-2xl p-8 text-white bg-size-[60%] bg-[url('./images/mechanicperson.webp')] bg-no-repeat bg-position-[right_-3em_bottom_-3em]  md:bg-position-[right_-4em_bottom_-3em] xl:bg-position-[right_-4em_top_-1em] xl:bg-size-[45%]!">
+  <div class="bg-blueFortune/80 flex flex-col justify-between min-h-62 h-full rounded-2xl p-8 text-white bg-size-[60%] bg-[url('/images/mechanicperson.webp')] bg-no-repeat bg-position-[right_-3em_bottom_-3em]  md:bg-position-[right_-4em_bottom_-3em] xl:bg-position-[right_-4em_top_-1em] xl:bg-size-[45%]!">
     <div>
       <span class="block text-blue-200 text-sm">Bem vindo!</span>
       <strong class="text-blue-50 text-xl">
